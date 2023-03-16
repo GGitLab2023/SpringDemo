e048c8968b4abd9a37c15e96b3d09176e652adc3
tejas.borse <tejas.borse@snsl-778.skillnetinc.com>
2023-03-16 16:23:26 +0530

    Change name tejas to tejas borse

--- src/main/java/com/simple/spring/PersonAppRun.java
+++ src/main/java/com/simple/spring/PersonAppRun.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class PersonAppRun {
 
 	public static void main(String[] args) {
-		ApplicationContext context =new ClassPathXmlApplicationContext("simplespring/simpleTaskletJob.xml");
+		ApplicationContext context =new ClassPathXmlApplicationContext("simplespring/simpleConfig.xml");
 		Person p=(Person)context.getBean("Person");
 		System.out.println("this is person id"+p.getPid());
 		System.out.println("this is person name"+p.getPname());
